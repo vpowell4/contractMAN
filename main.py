18228be73e2287ca00758604ea7378a365f07fef
ryan powell <vincentapowell@msn.com>
2020-05-12 16:22:41 +0100

    Various updates

--- main.py
+++ main.py
@@ -12,6 +12,10 @@ database = "contractmanager"
 def home():
     return render_template("home.html")
 
+@app.route("/config")
+def config():
+    return render_template("config.html")
+
 @app.route("/about")
 def about():
     return render_template("about.html")
@@ -25,30 +29,20 @@ def actors():
     columns=["userid","role","organisation","workgroup","telephone","email","adminlevel",
                 "active","createdt","updatedt"]
     cnxn = pyodbc.connect("Driver={SQL Server Native Client 11.0};Server="+server+
-                            ";Database="+database+
-                            ";Trusted_Connection=yes;")
+                            ";Database="+database+";Trusted_Connection=yes;")
     cursor = cnxn.cursor()
     data=cursor.execute('SELECT * FROM Actors FOR JSON PATH').fetchone()
     cursor.commit()
     cursor.close()
-    return render_template("actors.html",columns=columns,actordata="var actordata = "+data[0],
-                            actorcolumns="columns:[{title:'Userid', field:'userid'}, \
-                                {title:'Organisation', field:'organisation'}, \
-                                {title:'Role', field:'role'}, \
-                                {title:'Email', field:'email'}, \
-                                {title:'Admin Level', field:'adminlevel'}, \
-                                {title:'Active', field:'active',hozAlign:'center'}, \
-                                {title:'Updated', field:'updatedt', sorter:'date', hozAlign:'center'}]")
+    return render_template("actors.html",columns=columns,actordata=data[0])
 
 @app.route('/actor/upsert', methods=['POST'])
 def actorupsert():    
     data = request.get_json()
     cnxn = pyodbc.connect("Driver={SQL Server Native Client 11.0};Server="+server+
-                            ";Database="+database+
-                            ";Trusted_Connection=yes;")
+                            ";Database="+database+";Trusted_Connection=yes;")
     cursor = cnxn.cursor()
     sql = "EXECUTE ACTORS_UPSERT @JSONINFO='"+json.dumps(data)+"'"
-    print(sql)
     cursor.execute(sql)
     cursor.commit()
     cursor.close()
@@ -58,8 +52,7 @@ def actorupsert():
 def actordelete():    
     data = request.get_json()
     cnxn = pyodbc.connect("Driver={SQL Server Native Client 11.0};Server="+server+
-                            ";Database="+database+
-                            ";Trusted_Connection=yes;")
+                            ";Database="+database+";Trusted_Connection=yes;")
     cursor = cnxn.cursor()
     sql = "EXECUTE ACTORS_DELETE @EMAIL='"+data+"'"
     cursor.execute(sql)
